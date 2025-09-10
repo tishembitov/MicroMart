4d3b2eb1b03a119ec92702e1d8a8ccc60d5a0f85
tishembitov <tishembitov@yandex.ru>
2025-09-10 18:44:32 +0500

    refactor: fix test data

--- database/users-data.sql
+++ database/users-data.sql
@@ -1,6 +1,8 @@
 INSERT INTO users (created_at, email, name, password, updated_at, username)
-VALUES ('2023-08-01 08:00:00', 'johnDoe@example.com', 'John Doe',
-        'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', '2023-08-01 08:00:00', 'johndoe'),
-       ('2023-08-01 09:00:00', 'jabeSmith@example.com', 'Jane Smith',
-        'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', '2023-08-01 09:00:00', 'janesmith');
-
+VALUES
+    ('2023-08-01 08:00:00', 'johnDoe@example.com', 'John Doe',
+     '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG',
+     '2023-08-01 08:00:00', 'johndoe'),
+    ('2023-08-01 09:00:00', 'janeSmith@example.com', 'Jane Smith',
+     '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG',
+     '2023-08-01 09:00:00', 'janesmith');
